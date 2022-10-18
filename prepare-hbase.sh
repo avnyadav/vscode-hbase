1b3d78af890ea30faed3951bbb13bfe1cfc52ceb
Avnish327030 <anishyadav7045075175@gmail.com>
2022-10-18 18:06:54 +0530

    updaetd:

--- prepare-hbase.sh
+++ prepare-hbase.sh
@@ -8,4 +8,6 @@ apt-get install $minimal_apt_get_args $HBASE_BUILD_PACKAGES
 
 cd /opt
 
-curl -SL $HBASE_DIST/$HBASE_VERSION/hbase-$HBASE_VERSION-bin.tar.gz | tar -x -z && mv hbase-${HBASE_VERSION} hbase
+
+# curl -SL $HBASE_DIST/$HBASE_VERSION/hbase-$HBASE_VERSION-bin.tar.gz | tar -x -z && mv hbase-${HBASE_VERSION} hbase
+curl -SL https://archive.ineuron.ai/packages/hbase-2.2.4-bin.tar.gz | tar -x -z && mv hbase-${HBASE_VERSION} hbase
